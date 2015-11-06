9a55a5a00dcbcbbf234a1801dc6a91f161ece410
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-06 16:29:32 +0100

    Made config module, inline template

--- app.js
+++ app.js
@@ -1,31 +1,54 @@
-var module = angular.module('flapperNews', [])
-.factory('posts', [function(){
-  var o = {
-    posts: []
-  };
-  return o;
-}])
-.controller('MainCtrl', [
-'$scope', 'posts',
-function($scope, posts){
-$scope.posts = posts.posts;
-$scope.addPost = function(){
-  if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({
-    title: $scope.title,
-    link: $scope.link,
-    upvotes: 0
-  });
-  $scope.title = '';
-  $scope.link = '';
-};
+angular.module('flapperNews', ['ui.router'])
 
-$scope.incrementUpvotes = function  (post) {
-  post.upvotes += 1
-}
-$scope.title = '';
-$scope.link = '';
-}
+.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            });
+
+        $urlRouterProvider.otherwise('home');
+    }
+])
+
+
+.factory('posts', [
+
+    function() {
+        var o = {
+            posts: []
+        };
+        return o;
+    }
+])
+    .controller('MainCtrl', [
+        '$scope', 'posts',
+        function($scope, posts) {
+
+            $scope.posts = posts.posts;
+
+            $scope.addPost = function() {
+                if (!$scope.title || $scope.title === '') {
+                    return;
+                }
+                $scope.posts.push({
+                    title: $scope.title,
+                    link: $scope.link,
+                    upvotes: 0
+                });
+                $scope.title = '';
+                $scope.link = '';
+            };
+
+            $scope.incrementUpvotes = function(post) {
+                post.upvotes += 1
+            }
+            $scope.title = '';
+            $scope.link = '';
+        }
 ]);
-
-
