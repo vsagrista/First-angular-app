8dd15bd2f3495fcb59caac6280eae31840be1d51
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-06 09:55:24 +0100

    Added bootstrap, created factory module

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-angular.module('flapperNews', [])
+var module = angular.module('flapperNews', [])
+.factory('posts', [function(){
+  var o = {
+    posts: []
+  };
+  return o;
+}])
 .controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.posts = [
-  {title: 'post 1', upvotes: 5},
-  {title: 'post 2', upvotes: 2},
-  {title: 'post 3', upvotes: 15},
-  {title: 'post 4', upvotes: 9},
-  {title: 'post 5', upvotes: 4}
-];
+'$scope', 'posts',
+function($scope, posts){
+$scope.posts = posts.posts;
 $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({title: $scope.title, upvotes: 0});
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0
+  });
   $scope.title = '';
+  $scope.link = '';
 };
 
 $scope.incrementUpvotes = function  (post) {
-	post.upvotes += 1
+  post.upvotes += 1
 }
-
-
+$scope.title = '';
+$scope.link = '';
 }
-
 ]);
-
-
-
 
 
